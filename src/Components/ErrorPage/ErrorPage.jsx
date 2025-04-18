25da8a0d335dfd86366c378eebacec87299dc04b
imjoyhalder <joyhalder00113355@gamil.com>
2025-04-18 20:31:28 +0600

    navbar and footer section added

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className='text-5xl'>Page not Found</h1>
+            <p>Status: 404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,25 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './Components/Root/Root';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Home from './Components/Home/Home';
+import DashBoard from './Components/DashBoard/DashBoard';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path:'/dashboard',
+        element: <DashBoard></DashBoard>
+      }
+    ]
   },
 ]);
 
